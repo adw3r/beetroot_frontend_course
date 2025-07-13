9adb69b65b515da4bd6c4da112ccb5672f58c7a0
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-13 23:34:33 +0300

    rwd

--- rwd/assets/js/index.js
+++ rwd/assets/js/index.js
@@ -1,9 +1,11 @@
 import Lazyload from './lazyload.js';
-new Lazyload();
-document.toggleMenu = toggleMenu;
-function toggleMenu() {
+
+const lazyload = new Lazyload({});
+lazyload.update()
+
+const toggleMenu = () => {
     document.querySelectorAll('.js-hamburger').forEach(
-        function(el) {
+        function (el) {
             el.classList.toggle('is-active');
             let mobile_menu = document.querySelector('.mobile-menu');
             if (mobile_menu.classList.contains('open')) {
@@ -14,3 +16,26 @@ function toggleMenu() {
         }
     )
 }
+document.toggleMenu = toggleMenu;
+
+const getBgImageForWidth = (width) => {
+    if (width <= 425) {
+        return 'assets/images/phone/bg-image.jpg';
+    } else if (width <= 768) {
+        return 'assets/images/tablet/bg-image.jpg';
+    } else {
+        return 'assets/images/desktop/bg-image.jpg';
+    }
+};
+
+const bg_element = document.querySelector('.hero.lazy');
+
+function updateBg() {
+    bg_element.dataset.bg = getBgImageForWidth(window.innerWidth);
+    bg_element.removeAttribute('data-ll-status');        // если есть кастомные метки
+    lazyload.update();
+}
+
+window.addEventListener('resize', () => {
+    updateBg();
+});
