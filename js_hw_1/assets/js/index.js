86cdbb07f1e7f083989bf1279c4c3125809ba9f8
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-25 18:45:42 +0300

    js_hw_1

--- js_hw_1/assets/js/index.js
+++ js_hw_1/assets/js/index.js
@@ -1,9 +1,9 @@
 console.log('Hello World');
 
 // Minimum
-let varData;
-let var_ata;
-let VarData;
+// let varData;
+// let var_ata;
+// let VarData;
 /*
 let var-data; can`t be used
 let 213varData; can`t be used
@@ -11,51 +11,90 @@ let 213varData;
 */
 const CURRENT_YEAR = 2025;
 
+class UserName {
+    static USER_NAME_ELEMENT = '#userName';
 
-// Normal
-let askUserName = () => {
-    try {
+    static #validateUserName = (name) => {
+        if (!name?.trim()) {
+            alert('Username can`t be empty!');
+            return false;
+        }
 
-        let userName = prompt('Enter your name:');
-        if (!userName) {
-            alert('Username can`t be empty!')
-            return
+        if (!isNaN(Number(name))) {
+            alert('Username can`t be a number!');
+            return false;
         }
-        userName = userName[0].toUpperCase() + userName.slice(1);
-        document.querySelector('#userName').textContent = `Hello ${userName}!`;
-    } catch (error) {
-        console.log(error);
-        alert('Wrong input!');
-    }
-};
-document.getElementById('get_age_btn').addEventListener('click', () => {
-    try {
-        let year = prompt('Enter your year of birth:');
-        if (!year) {
-            alert('Year can`t be empty!')
-            return
-        }
-        year = +year;
-        if (isNaN(year)) {
-            alert('Not a number!');
-            return
-        }
-        document.querySelector('#age').textContent = `You are ${CURRENT_YEAR - year} years old.`;
-    } catch (error) {
-        console.log(error);
-        alert('Wrong input!');
-    }
-});
 
-const calculatePerimeter = (length) => {
-    if (!length) {
-        alert('Length can`t be empty!')
-        return
-    }
-    const perimeter = 4 * Number(length);
-    return perimeter;
+        return true;
+    };
+
+    static #capitalizeFirstLetter = (str) => {
+        return str[0].toUpperCase() + str.slice(1);
+    };
+
+    static ask = () => {
+        try {
+            const userName = prompt('Enter your name:');
+
+            if (!this.#validateUserName(userName)) {
+                return;
+            }
+
+            const formattedName = this.#capitalizeFirstLetter(userName);
+            document.querySelector(this.USER_NAME_ELEMENT).textContent = `Hello ${formattedName}!`;
+        } catch (error) {
+            console.error(error);
+            alert('Wrong input!');
+        }
+    };
 }
 
-const calculateRadius = (radius) => {
-    return undefined;
+class Age {
+    constructor() {
+        document.getElementById('get_age_btn').addEventListener('click', () => {
+            try {
+                let year = prompt('Enter your year of birth:');
+                if (!year) {
+                    alert('Year can`t be empty!')
+                    return
+                }
+                year = +year;
+                if (isNaN(year)) {
+                    alert('Not a number!');
+                    return
+                }
+                document.querySelector('#age').textContent = `You are ${CURRENT_YEAR - year} years old.`;
+            } catch (error) {
+                console.log(error);
+                alert('Wrong input!');
+            }
+        });
+    }
 }
+
+class Square {
+    constructor() {
+        document.getElementById('square_btn').addEventListener('click', () => {
+            try {
+                let length = document.getElementById('square_length').value
+                if (!length) {
+                    alert('Length can`t be empty!')
+                    return
+                }
+                length = +length;
+                if (isNaN(length)) {
+                    alert('Not a number!');
+                    return
+                }
+                const perimeter = 4 * Number(length);
+                document.getElementById('square_perimeter').textContent = `Perimeter: ${perimeter}`;
+            } catch (e) {
+                console.log(e);
+                alert('Wrong input!');
+            }
+        });
+    }
+}
+
+new Age()
+new Square()
