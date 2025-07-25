6123d5e6de1f2ee2cc356c1d552aecbd74e35a89
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-25 17:07:36 +0300

    js_hw_1

--- js_hw_1/assets/js/index.js
+++ js_hw_1/assets/js/index.js
@@ -9,15 +9,40 @@ let var-data; can`t be used
 let 213varData; can`t be used
 let 213varData;
 */
+const CURRENT_YEAR = 2025;
 
 
 // Normal
-const askUserName = () => {
-    let userName = prompt('Enter your name:');
-    if (!userName) {
-        alert('Username can`t be empty!')
-        return
+let askUserName = () => {
+    try {
+
+        let userName = prompt('Enter your name:');
+        if (!userName) {
+            alert('Username can`t be empty!')
+            return
+        }
+        userName = userName[0].toUpperCase() + userName.slice(1);
+        document.querySelector('#userName').textContent = `Hello ${userName}!`;
+    } catch (error) {
+        console.log(error);
+        alert('Wrong input!');
     }
-    userName = userName[0].toUpperCase() + userName.slice(1);
-    document.querySelector('#userName').textContent = `Hello ${userName}!`;
 };
+document.getElementById('get_age_btn').addEventListener('click', () => {
+    try {
+        let year = prompt('Enter your year of birth:');
+        if (!year) {
+            alert('Year can`t be empty!')
+            return
+        }
+        year = +year;
+        if (isNaN(year)) {
+            alert('Not a number!');
+            return
+        }
+        document.querySelector('#age').textContent = `You are ${CURRENT_YEAR - year} years old.`;
+    } catch (error) {
+        console.log(error);
+        alert('Wrong input!');
+    }
+});
