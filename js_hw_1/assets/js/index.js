308c6af3763520c73012f21ee904e560aaf28c9c
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-25 15:54:20 +0300

    js_hw_1

--- js_hw_1/assets/js/index.js
+++ js_hw_1/assets/js/index.js
@@ -1,1 +1,23 @@
 console.log('Hello World');
+
+// Minimum
+let varData;
+let var_ata;
+let VarData;
+/*
+let var-data; can`t be used
+let 213varData; can`t be used
+let 213varData;
+*/
+
+
+// Normal
+const askUserName = () => {
+    let userName = prompt('Enter your name:');
+    if (!userName) {
+        alert('Username can`t be empty!')
+        return
+    }
+    userName = userName[0].toUpperCase() + userName.slice(1);
+    document.querySelector('#userName').textContent = `Hello ${userName}!`;
+};
